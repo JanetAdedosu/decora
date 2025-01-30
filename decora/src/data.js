00fe30490c12da9d13effe07900289feabbd2303
JanetAdedosu <adedosuadeola71@gmail.com>
2025-01-30 15:02:42 +0100

    create array of products

--- decora/src/data.js
+++ decora/src/data.js
@@ -1,10 +1,126 @@
-const data{
+const data = {
     products: [
         {
-            name:
-            slug:
-            category:
-            image:
-        }
+            name:'Blau Sofa',
+            slug: 'Blau Sofa',
+            category:'Living room',
+            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            price: 260,
+            countInStock: 7,
+            brand: 'IKEA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'modern day sofa'
+        },
+        {
+            name:'Blau Sofa',
+            slug: 'Blau Sofa',
+            category:'Living room',
+            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            price: 260,
+            countInStock: 7,
+            brand: 'IKEA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'modern day sofa'
+        },
+        {
+            name:'Blau Sofa',
+            slug: 'Blau Sofa',
+            category:'Living room',
+            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            price: 260,
+            countInStock: 7,
+            brand: 'IKEA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'modern day sofa'
+        },
+        {
+            name:'Blau Sofa',
+            slug: 'Blau Sofa',
+            category:'Living room',
+            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            price: 260,
+            countInStock: 7,
+            brand: 'IKEA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'modern day sofa'
+        },
+        {
+            name:'Blau Sofa',
+            slug: 'Blau Sofa',
+            category:'Living room',
+            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            price: 260,
+            countInStock: 7,
+            brand: 'IKEA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'modern day sofa'
+        },
+        {
+            name:'Blau Sofa',
+            slug: 'Blau Sofa',
+            category:'Living room',
+            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            price: 260,
+            countInStock: 7,
+            brand: 'IKEA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'modern day sofa'
+        },
+        {
+            name:'Blau Sofa',
+            slug: 'Blau Sofa',
+            category:'Living room',
+            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            price: 260,
+            countInStock: 7,
+            brand: 'IKEA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'modern day sofa'
+        },
+        {
+            name:'Blau Sofa',
+            slug: 'Blau Sofa',
+            category:'Living room',
+            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            price: 260,
+            countInStock: 7,
+            brand: 'IKEA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'modern day sofa'
+        },
+        {
+            name:'Blau Sofa',
+            slug: 'Blau Sofa',
+            category:'Living room',
+            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            price: 260,
+            countInStock: 7,
+            brand: 'IKEA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'modern day sofa'
+        },
+        {
+            name:'Blau Sofa',
+            slug: 'Blau Sofa',
+            category:'Living room',
+            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            price: 260,
+            countInStock: 7,
+            brand: 'IKEA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'modern day sofa'
+        },
     ]
 }
+
+export default data;
