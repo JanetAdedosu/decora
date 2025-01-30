19bfbcc67892efa7b6207801451e54e364443e1f
JanetAdedosu <adedosuadeola71@gmail.com>
2025-01-30 15:37:57 +0100

    change products

--- decora/src/data.js
+++ decora/src/data.js
@@ -16,7 +16,7 @@ const data = {
             name:'Blau Sofa',
             slug: 'Blau Sofa',
             category:'Living room',
-            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            image:"./01F2F452-18D4-46F3-B5A2-9C8DE04F292E_1_105_c.jpeg",
             price: 260,
             countInStock: 7,
             brand: 'IKEA',
@@ -28,7 +28,7 @@ const data = {
             name:'Blau Sofa',
             slug: 'Blau Sofa',
             category:'Living room',
-            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            image:"./roberto-nickson-rEJxpBskj3Q-unsplash.jpg",
             price: 260,
             countInStock: 7,
             brand: 'IKEA',
@@ -39,8 +39,8 @@ const data = {
         {
             name:'Blau Sofa',
             slug: 'Blau Sofa',
-            category:'Living room',
-            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            category:'Kitchen section',
+            image:"./4DE8078D-A8D4-49F9-A835-3DE423CF29BF_1_105_c.jpeg",
             price: 260,
             countInStock: 7,
             brand: 'IKEA',
@@ -51,8 +51,8 @@ const data = {
         {
             name:'Blau Sofa',
             slug: 'Blau Sofa',
-            category:'Living room',
-            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            category:'Kitchen section',
+            image:"./7935DD7D-73A2-4CDF-B775-9FCC5F6A5F9C_1_105_c.jpeg",
             price: 260,
             countInStock: 7,
             brand: 'IKEA',
@@ -63,8 +63,8 @@ const data = {
         {
             name:'Blau Sofa',
             slug: 'Blau Sofa',
-            category:'Living room',
-            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            category:'Kichen section',
+            image:"./jason-briscoe-AQl-J19ocWE-unsplash.jpg",
             price: 260,
             countInStock: 7,
             brand: 'IKEA',
@@ -75,8 +75,8 @@ const data = {
         {
             name:'Blau Sofa',
             slug: 'Blau Sofa',
-            category:'Living room',
-            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            category:'Bedroom section',
+            image:"./A67513CE-B995-4F7E-A5FA-14F9682377B4_1_105_c.jpeg",
             price: 260,
             countInStock: 7,
             brand: 'IKEA',
@@ -87,8 +87,8 @@ const data = {
         {
             name:'Blau Sofa',
             slug: 'Blau Sofa',
-            category:'Living room',
-            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            category:'Bedroom section',
+            image:"./hutomo-abrianto-9ZvuWg8deho-unsplash.jpg",
             price: 260,
             countInStock: 7,
             brand: 'IKEA',
@@ -99,8 +99,8 @@ const data = {
         {
             name:'Blau Sofa',
             slug: 'Blau Sofa',
-            category:'Living room',
-            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            category:'Bedroom section',
+            image:"./prydumano-design-QOlRGOCQaC4-unsplash.jpg",
             price: 260,
             countInStock: 7,
             brand: 'IKEA',
@@ -111,14 +111,14 @@ const data = {
         {
             name:'Blau Sofa',
             slug: 'Blau Sofa',
-            category:'Living room',
-            image:"./B725C456-B472-4895-8804-BB1E57CEFA52_1_105_c.jpeg",
+            category:'Bathroom section',
+            image:"./curology-ycEKahEaO5U-unsplash.jpg",
             price: 260,
             countInStock: 7,
             brand: 'IKEA',
             rating: 4.5,
             numReviews: 10,
-            description: 'modern day sofa'
+            description: 'modern day tiles'
         },
     ]
 }
