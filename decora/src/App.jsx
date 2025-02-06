ebf9faf5317426ed9abea2e4763cb982d6c0d1c3
JanetAdedosu <adedosuadeola71@gmail.com>
2025-02-06 05:21:31 +0100

    new change

--- decora/src/App.jsx
+++ decora/src/App.jsx
@@ -14,16 +14,16 @@ function App() {
           <div className="products">
           {data.products.map((product)=> (
             <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
+              {/* <a href={`/product/${product.slug}`}>  */}
               <img src={product.image} alt={product.name}/>
-              </a>
+              {/* </a> */}
               <div className="product info">
-              <a href={`/product/${product.slug}`}>
+               <a href={`/product/${product.slug}`}> 
               <p>{product.name}</p>
               </a>
-              <a href={`/product/${product.slug}`}>
+              {/* <a href={`/product/${product.slug}`}> */}
               <p><strong>${product.price}</strong></p>
-              </a>
+              
               <button>Add To Cart</button>
               </div>
 
