cc278514a62fa706617c275092a70334d544e788
JanetAdedosu <adedosuadeola71@gmail.com>
2025-02-06 05:11:21 +0100

    create link for product image

--- decora/src/App.jsx
+++ decora/src/App.jsx
@@ -14,10 +14,16 @@ function App() {
           <div className="products">
           {data.products.map((product)=> (
             <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name}/>
+              </a>
               <div className="product info">
+              <a href={`/product/${product.slug}`}>
               <p>{product.name}</p>
-              <p>{product.price}</p>
+              </a>
+              <a href={`/product/${product.slug}`}>
+              <p><strong>${product.price}</strong></p>
+              </a>
               <button>Add To Cart</button>
               </div>
 
