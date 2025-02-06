5bf62e48ffed8eff262a5f054d88ffade855184d
JanetAdedosu <adedosuadeola71@gmail.com>
2025-02-06 03:31:01 +0100

    add italics

--- decora/src/App.jsx
+++ decora/src/App.jsx
@@ -6,16 +6,19 @@ function App() {
     
       <div>
         <header>
-          <a href ="/">decora</a>
+          <a href ="/"><em>DECORA</em></a>
         </header>
+        
         <main>
           <h1>Products</h1>
           <div className="products">
           {data.products.map((product)=> (
             <div className="product" key={product.slug}>
               <img src={product.image} alt={product.name}/>
+              <div className="product info">
               <p>{product.name}</p>
               <p>{product.price}</p>
+              </div>
 
             </div>))}
             </div>
